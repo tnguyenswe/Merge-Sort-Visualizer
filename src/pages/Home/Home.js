dc4af1174cf7bcf31b918dd5aa83208f7d571bea
Thomas Nguyen <tnguyenswe@gmail.com>
2023-05-09 18:45:01 -0700

    Add theme UI package + styling changes

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,13 @@
+/** @jsxImportSource theme-ui */
 import React, { useState, useEffect } from "react";
 import "./Home.css";
-import {getMergeSortAnimations} from '../Algorithms/algorithms.js';
+import { getMergeSortAnimations } from '../Algorithms/algorithms.js';
 
 // Change this value for the speed of the animations.
 const ANIMATION_SPEED_MS = 1;
 
 // This is the main color of the array bars.
-const PRIMARY_COLOR = 'turquoise';
+const PRIMARY_COLOR = '#3756D3';
 
 // This is the color of array bars that are being compared throughout the animations.
 const SECONDARY_COLOR = 'red';
@@ -14,40 +15,50 @@ const SECONDARY_COLOR = 'red';
 const Home = (props) => {
     const [myList, setMyList] = useState([]);
 
-    const displayList = (e) => {
+    const printList = (e) => {
         e.preventDefault();
         console.log(myList);
     }
 
     const mergeSort = () => {
+        // We get the animations array based on our algorithm's output.
         const animations = getMergeSortAnimations(myList);
+        // Loop through the animations array to perform animations.
         for (let i = 0; i < animations.length; i++) {
-          const arrayBars = document.getElementsByClassName('array-item-container');
-          const isColorChange = i % 3 !== 2;
-          if (isColorChange) {
-            const [barOneIdx, barTwoIdx] = animations[i];
-            const barOneStyle = arrayBars[barOneIdx].style;
-            const barTwoStyle = arrayBars[barTwoIdx].style;
-            const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
-            setTimeout(() => {
-              barOneStyle.backgroundColor = color;
-              barTwoStyle.backgroundColor = color;
-            }, i * ANIMATION_SPEED_MS);
-          } else {
-            setTimeout(() => {
-              const [barOneIdx, newHeight] = animations[i];
-              const barOneStyle = arrayBars[barOneIdx].style;
-              barOneStyle.height = `${newHeight}px`;
-            }, i * ANIMATION_SPEED_MS);
-          }
+            // This gets the array items
+            const arrayItems = document.getElementsByClassName('array-item-container');
+            // We update the color if the index modulus 3 is not equal 2.
+            // This is because we only want to animate the first and second values being compared.
+            // 0 % 3 = 0, 1 % 3 = 1, 2 % 3 = 2, so it would animate 0 and 1, but not 2.
+            const updateColor = i % 3 !== 2;
+            if (updateColor) {
+                // Get the indices of the values being animated.
+                const [barOneIndex, barTwoIndex] = animations[i];
+                const barOneStyle = arrayItems[barOneIndex].style;
+                const barTwoStyle = arrayItems[barTwoIndex].style;
+                // Update the color to be either primary or secondary color.
+                const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+                // We use a time out function to determine how long the array item is a different color.
+                setTimeout(() => {
+                    barOneStyle.backgroundColor = color;
+                    barTwoStyle.backgroundColor = color;
+                }, i * ANIMATION_SPEED_MS);
+            } else {
+                // This is the case when the value of a comparison needs to be updated.
+                setTimeout(() => {
+                    const [barOneIndex, newHeight] = animations[i];
+                    const barOneStyle = arrayItems[barOneIndex].style;
+                    barOneStyle.height = `${newHeight}px`;
+                }, i * ANIMATION_SPEED_MS);
+            }
         }
-      }
+    }
 
     // Function to generate randomized array of size arraySize.
     const randomizeArray = () => {
         const arraySize = 100;
         const tempArray = [];
-        for(let i=0; i<arraySize; i++){
+        for (let i = 0; i < arraySize; i++) {
             tempArray.push(randomIntFromInterval(10, 100));
         }
         setMyList(tempArray);
@@ -64,26 +75,75 @@ const Home = (props) => {
     }, []);
 
     return (
-        <div className="container-div">
-            <h1 className="title-text">
-                Sorting Algorithm Visualizer
+        <div 
+        sx={{
+            display: 'flex',
+            flexDirection: 'column',
+        }}
+        >
+            <h1 sx={{ fontSize: '24px', pl: '24px', pt: '10px' }} className="title-text">
+                Sorting Algorithm <span sx={{ color: 'navy10' }}>Visualizer</span>
             </h1>
 
-            <button onClick={randomizeArray}>
+            <div sx={{display: 'flex', mt: '20px'}}>
+            <button
+                sx={{
+                    backgroundColor: 'navy10',
+                    border: 'none',
+                    borderRadius: '50px',
+                    padding: '10px',
+                    color: 'white',
+                    ml: '24px',
+                    width: '150px',
+                    alignSelf: 'center',
+                    mr: '10px'
+                }}
+                onClick={randomizeArray}>
                 Randomize Array
             </button>
 
-            <button onClick={displayList}>
-                Display List
+            <button
+                sx={{
+                    backgroundColor: 'navy10',
+                    border: 'none',
+                    borderRadius: '50px',
+                    padding: '10px',
+                    color: 'white',
+                    alignSelf: 'flex-start',
+                    mr: '10px',
+                }}
+                className="buttons" onClick={printList}>
+                Console Log List
             </button>
 
-            <button onClick={mergeSort}>
+            <button
+                sx={{
+                    backgroundColor: 'navy10',
+                    border: 'none',
+                    borderRadius: '50px',
+                    padding: '10px',
+                    color: 'white',
+                    alignSelf: 'flex-start',
+                }}
+                className="buttons" onClick={mergeSort}>
                 Merge Sort
             </button>
 
-            <div className="array-div">
+            </div>
+
+            <div
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'space-around',
+                    alignItems: 'end',
+                    width: '80%',
+                    justifySelf: 'center',
+                    mt: '30px',
+                    ml: '24px',
+                }}
+            >
                 {myList.map((val, index) => {
-                    return <div className="array-item-container" key={index} style={{height: `${val}px`, backgroundColor: PRIMARY_COLOR}} ></div>
+                    return <div className="array-item-container" key={index} style={{ height: `${val}px`, backgroundColor: PRIMARY_COLOR }} ></div>
                 })}
             </div>
         </div>
